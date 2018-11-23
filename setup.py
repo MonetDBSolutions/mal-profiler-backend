3af23ac337d0a63fbdd5be4d125961c71dc1f282
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2018-11-23 11:14:36 +0100

    Make sure that SQL files are included

--- setup.py
+++ setup.py
@@ -5,5 +5,11 @@ setup(
     version="0.1.dev0",
     packages=find_packages(),
     setup_requires=['pytest-runner'],
-    tests_require=['pytest']
+    tests_require=['pytest'],
+    include_package_data=True,
+    # data_files=[
+    #     ('./data', ['mal_profiler/data/tables.sql',
+    #                            'mal_profiler/data/add_constraints.sql',
+    #                            'mal_profiler/data/drop_constraints.sql'])
+    # ]
 )
