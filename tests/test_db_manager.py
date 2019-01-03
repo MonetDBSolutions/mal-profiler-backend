d62d459daf4cf970a8be5e440288b04d78e3331e
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2019-01-03 17:17:44 +0100

    Happy new year

--- tests/test_db_manager.py
+++ tests/test_db_manager.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Copyright MonetDB Solutions B.V. 2018
+# Copyright MonetDB Solutions B.V. 2018-2019
 
 import pytest
 
