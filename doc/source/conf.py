377b72187179cc621b34374f8a5bc6dd9623594c
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2019-02-21 15:50:02 +0100

    Bump version to 0.3.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, MonetDB Solutions B.V.'
 author = 'Panagiotis Koutsourakis'
 
 # The short X.Y version
-version = '0.2'
+version = '0.3'
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
