e2d84ebbc8cbcc28ded3f4df888168943c5497f5
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2019-02-22 15:14:00 +0100

    Update copyright notice in documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'MAL Analytics'
-copyright = '2018, MonetDB Solutions B.V.'
+copyright = '2018-2019, MonetDB Solutions B.V.'
 author = 'Panagiotis Koutsourakis'
 
 # The short X.Y version
